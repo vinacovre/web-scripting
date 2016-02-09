8a90a52d2f98891bcfd0f7c10cbe8aa282c7590d
vinacovre <viniassis@me.com>
2016-02-09 00:46:58 -0500

    add 2nd js function (pic shaddow)  & jQuery from http://jsfiddle.net/gilbitron/Lt2wH/

--- HW1/myScript.js
+++ HW1/myScript.js
@@ -2,3 +2,16 @@ function redirect(url)
 {
     window.open(url, '_blank');
 }
+
+function shadow(verif)
+{
+    var profilePic = document.getElementsByClassName('pic')[0];
+    if(verif)
+    {
+        profilePic.setAttribute("class", "pic picShadow");
+    }
+    else
+    {
+        profilePic.setAttribute("class", "pic");
+    }
+}
