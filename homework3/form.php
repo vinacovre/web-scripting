ca85546b046d1835bd01330bd7abba4817ff51ef
vinacovre <viniassis@me.com>
2016-02-22 22:10:12 -0500

    add HW3

---
+++ homework3/form.php
@@ -0,0 +1,23 @@
+<DOCTYPE! html>
+<html>
+  </head>
+    <meta charset="utf-8">
+    <title>Interacting With Database</title>
+  </head>
+
+  <body>
+    <h1 style="text-align:center">Blog</h1>
+
+    <h2>Post</h2>
+    <form>
+      Title:<br>
+      <input type="text" name="post_title" size="102"><br>
+      Content:<br>
+      <textarea name="post_content" rows="20" cols="100">
+      </textarea>
+      <br>
+      <input type="submit">
+    </form>
+
+  </body>
+</html>
